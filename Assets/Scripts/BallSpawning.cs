70e42a0d484882e86e47c7f7e4e0d2af35a6cb7b
KiLAR <dpaul3333@gmail.com>
2024-04-08 10:20:55 +0530

    Update BallSpawning.cs

--- Assets/Scripts/BallSpawning.cs
+++ Assets/Scripts/BallSpawning.cs
@@ -14,18 +14,15 @@ public class BallSpawning : MonoBehaviour
 
     void SpawnObjects()
     {
-        if (objectsToSpawn.Length == 0 || spawnPoints.Length == 0)
+      /*  if (objectsToSpawn.Length == 0 || spawnPoints.Length == 0)
         {
             Debug.LogWarning("No objects to spawn or no spawn points specified.");
             return;
-        }
+        }*/
 
         for (int i = 0; i < objectsToSpawn.Length; i++)
         {
-            // Choose a random spawn point
             Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
-
-            // Instantiate the object at the chosen spawn point
             Instantiate(objectsToSpawn[i], spawnPoint.position, spawnPoint.rotation);
         }
     }
